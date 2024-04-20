b97e8c1520f1c219f032b611194f7b29eae37b82
ana carla <159553141+anc4rla@users.noreply.github.com>
2024-04-20 00:43:45 -0300

    commit_noturno

---
+++ atividade.js
@@ -0,0 +1,15 @@
+const nome = "Helena";
+const sexo = "F";
+const idade = 44;
+const contribuição = 26;
+
+const contribuiçãoMínima = 30;
+
+const somaIC = idade + contribuição
+
+if (contribuição >= contribuiçãoMínima) {
+    if (somaIC >= 85) { console.log("Helena, você pode se aposentar!");
+    } else {console.log("Helena, você não pode se aposentar!")
+    }
+    } else {console.log("Helena, você não pode se aposentar!")
+    }
